106aaee12d1526b32fd4c246958a0dee0a97bb29
BuildTools <unconfigured@null.spigotmc.org>
2024-03-24 22:58:39 -0400

    Updated Detector.py to add variable tolerance.

--- ObjDetection_Current/Detector.py
+++ ObjDetection_Current/Detector.py
@@ -13,11 +13,18 @@ print("Arduino active..")
 
 global_servo_positionX = 90
 global_servo_positionY = 90
+global_position_tolerance = 0
+
+cv.namedWindow('Tolerance Selector')
+TolWin = np.zeros((300,512,3), np.uint8)
 
 cap = cv.VideoCapture(0)
 
 if not cap.isOpened():
     exit("Unable to open camera..")
+
+def GetToleranceLimit(Tolerance):
+    print(Tolerance)
 
 def moveServoX(pos):
     print(f"Ran moveServoX {pos}")
@@ -42,7 +49,7 @@ def DetectObj(frame):
             diffx = abs(int(rectx) - center_x)
             diffy = abs(int(recty) - center_y)
             
-            if diffx <= 10:
+            if diffx <= global_position_tolerance:
                 print("Aligned on X axis")
             else:
                 if int(rectx) > center_x:
@@ -50,7 +57,7 @@ def DetectObj(frame):
                 elif int(rectx) < center_x:
                     global_servo_positionX -= 1
             
-            if diffy <= 10:
+            if diffy <= global_position_tolerance:
                 print("Aligned on the Y axis")
             else:
                 if int(recty) > center_y:
@@ -58,13 +65,13 @@ def DetectObj(frame):
                 elif int(recty) < center_y:
                     global_servo_positionY -= 1
                     
-            if diffx and diffy <= 20:
+            if diffx and diffy <= global_position_tolerance:
                 print("Both aligned")
                 
             global_servo_positionX = max(0, min(180, global_servo_positionX))
             global_servo_positionY = max(0, min(180, global_servo_positionY))
             cv.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
-            cv.circle(frame,rectcenter,5,(0,255,0),2)
+            cv.circle(frame,rectcenter,global_position_tolerance,(0,255,0),2)
             cv.line(frame, (center_x, 0), (center_x, F_height), (0, 255, 0), 1)
             cv.line(frame, (0, center_y), (F_width, center_y), (0, 255, 0), 1)
             
@@ -77,13 +84,19 @@ def DetectObj(frame):
             for th in threads:
                 th.join()
 
+cv.createTrackbar('Tolerance','Tolerance Selector',0,150,GetToleranceLimit)
+
 def Show_Cap():
+    global global_position_tolerance
     while cap.isOpened():
         ret, frame = cap.read()
         
         if not ret:
             exit("Unable to read video feed")
-            
+        
+        cv.imshow("Tolerance Selector",TolWin)
+        global_position_tolerance = cv.getTrackbarPos('Tolerance','Tolerance Selector')
+        
         threading.Thread(target=DetectObj(frame)).start()
         cv.imshow("Video", frame)
         if cv.waitKey(1) == ord("q"):
